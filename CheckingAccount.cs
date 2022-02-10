c852e5740194d1b84a95655dcfb62b8c8b911c9f
nta228 <64982003+nta228@users.noreply.github.com>
2022-02-10 16:47:30 +0700

    Create CheckingAccount.cs

---
+++ CheckingAccount.cs
@@ -0,0 +1,9 @@
+namespace BankExcercise.Accounts
+{
+public abstract class CheckingAccount : Account
+{
+    public CheckingAccount(int owner, decimal balance) : base(owner, balance)
+    {
+    }
+}
+}
